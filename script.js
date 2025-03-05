715c77eeedbbdda51d761f694c0744fad39b3eb0
kudaT <timilehinkudaisi01@gmail.com>
2025-03-05 05:35:20 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,42 @@
+const books = [
+  {
+    title: "Little Women",
+    image: "/images/littlewomen.png",
+  },
+  {
+    title: "A Christmas Tale",
+    image: "/images/christmastale.png",
+  },
+  {
+    title: "How to Stop Worrying and Start Living",
+    image: "/images/stopworrying.png",
+  },
+  {
+    title: "The Amazing Results of Positive Thinking",
+    image: "/images/amazingresults.png",
+  },
+  {
+    title: "Hot Water",
+    image: "/images/hotwater.png",
+  },
+  {
+    title: "Sea of Poppies",
+    image: "/images/seaofpoppies.png",
+  },
+  {
+    title: "The Monk Who Sold His Ferrari",
+    image: "/images/monkferari.png",
+  },
+  {
+    title: "Pride and Prejudice",
+    image: "/images/prideandprejudice.png",
+  },
+  {
+    title: "Principle Centered Leadership",
+    image: "/images/pcl.png",
+  },
+  {
+    title: "The Alchemist",
+    image: "/images/thealchemist.png",
+  },
+];
